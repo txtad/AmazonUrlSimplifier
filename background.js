2cf46d72d1714f3fab2b00202628e57af6370123
Tad Marko <tmarko@fueland.com>
2024-10-11 14:15:39 -0500

    Now operates when the toolbar icon is clicked

--- background.js
+++ background.js
@@ -1,3 +1,17 @@
 chrome.action.onClicked.addListener((tab) => {
-  chrome.tabs.sendMessage(tab.id, {action: "simplifyLink"});
+  if (tab.url.includes("amazon.com")) {
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: simplifyAmazonLink
+    });
+  }
 });
+
+function simplifyAmazonLink() {
+  let url = window.location.href;
+  let match = url.match(/\/dp\/([A-Z0-9]{10})/);
+  if (match) {
+    let simplifiedUrl = `https://amazon.com/dp/${match[1]}/`;
+    window.location.href = simplifiedUrl;
+  }
+}
